6d80954620ddc977140264b03eed6163f9714587
Johnny Squardo <johnnysquardo@gmail.com>
2025-05-06 16:44:00 -0400

    Add shuffle function

---
+++ lib/card_deck.ex
@@ -0,0 +1,22 @@
+defmodule CardDeck do
+  @moduledoc """
+  A simple library for creating and manipulating a standard 52-card deck.
+  """
+
+  @suits [:hearts, :diamonds, :clubs, :spades]
+  @ranks ~w(2 3 4 5 6 7 8 9 10 J Q K A)a
+
+  @doc """
+  Returns a new, ordered deck of 52 cards
+  """
+  def new_deck do
+    for suit <- @suits, rank <- @ranks do
+      {rank, suit}
+    end
+  end
+
+  @doc """
+  Shuffles the given deck of cards.
+  """
+
+end
